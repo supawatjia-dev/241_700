edf2e0ce111a267edabcffac55d36c5394a3b72b
supawatjia-dev <supawat.jia@ku.th>
2026-03-06 12:45:58 +0700

    FrontEnd ==> MySQL

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,29 +1,83 @@
+const validateDate = (userData) => {
+    let errors = [];
+    if (!userData.firstname){
+        errors.push('กรุณากรอกชื่อ')
+    }
+    if (!userData.lastname){
+        errors.push('กรุณากรอกนามสกุล')
+    }
+    if (!userData.age){
+        errors.push('กรุณากรอกอายุ')
+    }
+    if (!userData.gender){
+        errors.push('กรุณาเลือกเพศ')
+    }
+    if (!userData.interests){
+        errors.push('กรุณาเลือกงานอดิเรก')
+    }
+    if (!userData.description){
+        errors.push('กรุณากรอกคำอธิยาย')
+    }
+    return errors;
+}
 const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
+    let genderDOM = document.querySelector('input[name=gender]:checked') || {};
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked') || {};
     let descriptionDOM = document.querySelector('textarea[name=description]');
+    let messageDOM = document.getElementById('message')
 
-    let interest = ''
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value 
-        if (i != interestDOMs.length - 1) {
-            interest += ','
+    try {
+        let interest = ''
+        for (let i = 0; i < interestDOMs.length; i++) {
+            interest += interestDOMs[i].value
+            if (i != interestDOMs.length - 1) {
+                interest += ','
+            }
+        }
+
+
+        let userData = {
+            firstname: firstNameDOM.value,
+            lastname: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
+        }
+        console.log('submitData', userData);
+        
+    const errors = validateDate(userData);
+    if (errors.length > 0){
+        throw {
+            message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+            errors: errors
         }
     }
 
+        const response = await axios.post('http://localhost:8000/users', userData);
+        console.log('response', response)
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDOM.className = 'message success'
+    } catch (error) {
+        console.log('error message', error.message);
+        console.log('error detail',error.errors);
+        //if (error.response) {
+        //    console.log('Error response:', error.response.data.message)
+        //}
+        let htmlData = '<div>'
+        htmlData += `<div>${error.message}</div>`;
+        htmlData += '<ul>'
+        for (let i = 0 ; i<error.errors.length ; i++){
+            htmlData += `<li>${error.errors[i]}</li>`;
+        }
+        htmlData += '</ul>';
+        htmlData += '</div>';
 
-    let userData ={
-        firstname: firstNameDOM.value,
-        lastname: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interests: interest
+
+        messageDOM.innerHTML = htmlData;
+        messageDOM.className = 'message danger';
     }
-     console.log('submitData', userData);
-     const response = await axios.post('http://localhost:8000/users',userData);
-     console.log('response',response)
 }
